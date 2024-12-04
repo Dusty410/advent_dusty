076dacfa2d2da2a95ac6de91cf8ef7855cda308c
David Simpson <david.simpson@frayt.com>
2024-12-04 16:03:00 -0500

    day2p2 solved, added day 3

--- lib/advent/year2024/day02.ex
+++ lib/advent/year2024/day02.ex
@@ -37,57 +37,72 @@ defmodule Advent.Year2024.Day02 do
   end
 
   defp ascending_check_with_safety(list) do
-    list
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.all?(fn x ->
-      first = Enum.fetch!(x, 0)
-      last = Enum.fetch!(x, 1)
+    result =
+      list
+      |> Enum.with_index()
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.find(fn x ->
+        first_with_index = Enum.fetch!(x, 0)
+        last_with_index = Enum.fetch!(x, 1)
 
-      if first < last and abs(first - last) >= 1 and abs(first - last) <= 3 do
+        first = elem(first_with_index, 0)
+        last = elem(last_with_index, 0)
+
+        not (first < last and abs(first - last) >= 1 and abs(first - last) <= 3)
+      end)
+
+    case result do
+      nil ->
         true
-      else
-        list |> List.delete(first) |> ascending_check() ||
-          list |> List.delete(last) |> ascending_check()
-      end
-    end)
+
+      _ ->
+        first_index = Enum.fetch!(result, 0) |> elem(1)
+        last_index = Enum.fetch!(result, 1) |> elem(1)
+
+        list |> List.delete_at(first_index) |> ascending_check() ||
+          list |> List.delete_at(last_index) |> ascending_check()
+    end
   end
 
   defp descending_check_with_safety(list) do
     result =
       list
+      |> Enum.with_index()
       |> Enum.chunk_every(2, 1, :discard)
-      |> Enum.all?(fn x ->
-        first = Enum.fetch!(x, 0)
-        last = Enum.fetch!(x, 1)
+      |> IO.inspect(charlists: :as_lists, limit: :infinity)
+      |> Enum.find(fn x ->
+        first_with_index = Enum.fetch!(x, 0)
+        last_with_index = Enum.fetch!(x, 1)
 
-        first > last and abs(first - last) >= 1 and abs(first - last) <= 3
-        # if first > last and abs(first - last) >= 1 and abs(first - last) <= 3 do
-        #   true
-        # else
-        #   list |> List.delete(first) |> descending_check() ||
-        #     list |> List.delete(last) |> descending_check()
-        # end
+        first = elem(first_with_index, 0)
+        last = elem(last_with_index, 0)
+
+        not (first > last and abs(first - last) >= 1 and abs(first - last) <= 3)
       end)
 
-      # if result 
-  end
+    case result do
+      nil ->
+        true
 
-  # defp success_or_culprits(list) do
-  #   
-  # end
+      _ ->
+        first_index = Enum.fetch!(result, 0) |> elem(1)
+        last_index = Enum.fetch!(result, 1) |> elem(1)
+
+        list |> List.delete_at(first_index) |> descending_check() ||
+          list |> List.delete_at(last_index) |> descending_check()
+    end
+  end
 
   def part2(_) do
     day = 2
 
     day
     |> Advent.Input.get!()
-    |> String.split("\n")
-    |> Enum.reverse()
-    |> tl()
-    |> Enum.reverse()
-    |> Enum.map(fn x ->
-      String.split(x) |> Enum.map(fn y -> String.to_integer(y) end)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn list ->
+      String.split(list) |> Enum.map(fn elem -> String.to_integer(elem) end)
     end)
+    # |> IO.inspect(charlists: :as_lists, limit: :infinity)
     |> Enum.count(fn list ->
       ascending_check_with_safety(list) or descending_check_with_safety(list)
     end)
