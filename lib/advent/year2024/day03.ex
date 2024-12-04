7c9858593925754eeafcabace9e742933290c0d4
David Simpson <david.simpson@frayt.com>
2024-12-04 17:33:49 -0500

    day 3 solved

--- lib/advent/year2024/day03.ex
+++ lib/advent/year2024/day03.ex
@@ -1,9 +1,46 @@
 defmodule Advent.Year2024.Day03 do
-  def part1(args) do
-    args
+  def part1(_) do
+    day = 3
+
+    input = Advent.Input.get!(day)
+
+    Regex.scan(~r/mul\([0-9]*,[0-9]*\)/, input)
+    |> List.flatten()
+    |> Enum.map(fn oper ->
+      [a, b] = Regex.scan(~r/[0-9]+/, oper) |> List.flatten()
+
+      String.to_integer(a) * String.to_integer(b)
+    end)
+    |> Enum.sum()
   end
 
-  def part2(args) do
-    args
+  def part2(_) do
+    day = 3
+
+    input = Advent.Input.get!(day)
+
+    Regex.scan(~r/mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)/, input)
+    |> List.flatten()
+    |> Enum.reduce(%{do: true, acc: []}, fn element, %{do: do?, acc: acc} ->
+      if do? do
+        case element do
+          "do()" -> %{do: true, acc: acc}
+          "don't()" -> %{do: false, acc: acc}
+          _ -> %{do: true, acc: [element | acc]}
+        end
+      else
+        case element do
+          "do()" -> %{do: true, acc: acc}
+          _ -> %{do: false, acc: acc}
+        end
+      end
+    end)
+    |> Map.get(:acc)
+    |> Enum.map(fn oper ->
+      [a, b] = Regex.scan(~r/[0-9]+/, oper) |> List.flatten()
+
+      String.to_integer(a) * String.to_integer(b)
+    end)
+    |> Enum.sum()
   end
 end
