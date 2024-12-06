a10e31e1aff8ff4123d542ad16f9ef40adc92498
David Simpson <david.simpson@frayt.com>
2024-12-06 16:12:01 -0500

    generated day 6

---
+++ test/advent/year2024/day06_test.exs
@@ -0,0 +1,21 @@
+defmodule Advent.Year2024.Day06Test do
+  use ExUnit.Case
+
+  import Elixir.Advent.Year2024.Day06
+
+  @tag :skip
+  test "part1" do
+    input = nil
+    result = part1(input)
+
+    assert result
+  end
+
+  @tag :skip
+  test "part2" do
+    input = nil
+    result = part2(input)
+
+    assert result
+  end
+end
