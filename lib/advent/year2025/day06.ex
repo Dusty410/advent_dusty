22f853f40472f8dc8f360e850bfe943a1dd20117
Dusty410 <45187978+Dusty410@users.noreply.github.com>
2025-12-06 21:33:53 -0500

    2025 day06 p2 done

--- lib/advent/year2025/day06.ex
+++ lib/advent/year2025/day06.ex
@@ -43,5 +43,48 @@ defmodule Advent.Year2025.Day06 do
 
   def part2(args) do
     args
+    |> condition_input_complex()
+    |> Enum.map(fn equation ->
+      equation
+      |> perform_operation()
+    end)
+    |> Enum.sum()
+  end
+
+  defp condition_input_complex(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&(String.reverse(&1) |> String.split("", trim: true)))
+    |> Enum.zip()
+    |> Enum.map(fn tuple ->
+      tuple |> Tuple.to_list() |> List.to_string() |> String.trim()
+    end)
+    |> Enum.chunk_by(&(&1 == ""))
+    |> Enum.reject(&(&1 == [""]))
+    |> Enum.map(fn terms ->
+      operator_string = terms |> List.last() |> String.last()
+
+      operation =
+        case operator_string do
+          "*" -> &Kernel.*/2
+          "+" -> &Kernel.+/2
+        end
+
+      terms =
+        terms
+        |> Enum.map(fn term ->
+          if String.contains?(term, operator_string) do
+            term
+            |> String.split(operator_string)
+            |> hd()
+            |> String.trim()
+            |> String.to_integer()
+          else
+            term |> String.trim() |> String.to_integer()
+          end
+        end)
+
+      {terms, operation}
+    end)
   end
 end
