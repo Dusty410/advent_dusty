6f7ab1844c31a957b41880d452f7cb01a2c937a6
Dusty410 <45187978+Dusty410@users.noreply.github.com>
2025-12-09 08:58:44 -0500

    day 8 pt 1, failed attempt

--- lib/advent/year2025/day08.ex
+++ lib/advent/year2025/day08.ex
@@ -1,6 +1,98 @@
 defmodule Advent.Year2025.Day08 do
-  def part1(args) do
+  @type xyz :: {non_neg_integer(), non_neg_integer(), non_neg_integer()}
+  @type point_pair :: {xyz(), xyz()}
+  # %{shortest_pairs: list of pairs and distances, circuits: list of lists of points, e.g. [[xyz, xyz], [xyz]]}
+
+  def part1(args, limit \\ 1000) do
     args
+    |> condition_input()
+    |> find_nearest_pairs([], limit)
+  end
+
+  defp condition_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn xyz ->
+      xyz
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+  end
+
+  # @spec all_unique_pairs(points :: list(xyz())) :: list({xyz(), xyz()})
+  # defp all_unique_pairs(points) do
+  #   Enum.reduce(points, [], fn point1, acc ->
+  #     Enum.reduce(points, acc, fn point2, acc ->
+  #       if {point1, point2} in acc or {point2, point1} in acc,
+  #         do: acc,
+  #         else: [{point1, point2} | acc]
+  #     end)
+  #   end)
+  # end
+
+  @spec calc_distance(xyz(), xyz()) :: float()
+  defp calc_distance({x1, y1, z1}, {x2, y2, z2}) do
+    # IO.puts("Calculating distance between (#{x1},#{y1},#{z1} and (#{x2},#{y2},#{z2})")
+
+    :math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+  end
+
+  @spec find_nearest_pairs(
+          points :: list(xyz()),
+          found :: list({point_pair(), float()}),
+          limit :: non_neg_integer()
+        ) :: list({point_pair(), float()})
+  defp find_nearest_pairs(points, found_pairs, limit)
+
+  defp find_nearest_pairs(_points, found_pairs, limit) when length(found_pairs) >= limit,
+    do: found_pairs
+
+  defp find_nearest_pairs(points, found_pairs, already_checked, limit) do
+    for point1 <- points, point2 <- points, reduce: [] do
+      acc ->
+    end
+    # next_shortest =
+    # Enum.reduce(points, nil, fn point1, acc ->
+    #   Enum.reduce(points, acc, fn point2, acc ->
+    #     # acc = if is_nil(acc), do: {{point1, point2}, calc_distance(point1, point2)}, else: acc
+    #
+    #     cond do
+    #       point1 == point2 ->
+    #         acc
+    #
+    #       is_nil(acc) ->
+    #         {{point1, point2}, calc_distance(point1, point2)}
+    #
+    #       point_pair_already_found?(found_pairs, {point1, point2}) ->
+    #         acc
+    #
+    #       true ->
+    #         {_current_shortest_pair, current_shortest_distance} = acc
+    #         new_distance = calc_distance(point1, point2)
+    #
+    #         if new_distance < current_shortest_distance,
+    #           do: {{point1, point2}, new_distance},
+    #           else: acc
+    #     end
+    #   end)
+    # end)
+
+    # found_pairs = [next_shortest | found_pairs]
+    #
+    # found_pairs |> length() |> IO.inspect()
+    #
+    # find_nearest_pairs(points, found_pairs, limit)
+  end
+
+  @spec point_pair_already_found?(found_pairs :: list({point_pair(), float()}), point_pair()) ::
+          boolean()
+  defp point_pair_already_found?(found_pairs, point_pair) do
+    {p1, p2} = point_pair
+
+    Enum.any?(found_pairs, fn {found_point_pair, _distance} ->
+      point_pair == found_point_pair or {p2, p1} == found_point_pair
+    end)
   end
 
   def part2(args) do
--- test/advent/year2025/day08_test.exs
+++ test/advent/year2025/day08_test.exs
@@ -3,12 +3,13 @@ defmodule Advent.Year2025.Day08Test do
 
   import Elixir.Advent.Year2025.Day08
 
-  @input ""
+  @input "162,817,812\n57,618,57\n906,360,560\n592,479,940\n352,342,300\n466,668,158\n542,29,236\n431,825,988\n739,650,466\n52,470,668\n216,146,977\n819,987,18\n117,168,530\n805,96,715\n346,949,466\n970,615,88\n941,993,340\n862,61,35\n984,92,344\n425,690,689\n"
+  @limit 10
 
   test "part1" do
-    result = part1(@input)
+    result = part1(@input, @limit)
 
-    assert result
+    assert result == 40
   end
 
   test "part2" do
