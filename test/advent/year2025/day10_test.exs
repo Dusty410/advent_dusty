5cfb76c8abf12c971ea36a2cb11295ce1b93b207
Dusty410 <45187978+Dusty410@users.noreply.github.com>
2025-12-13 22:06:18 -0500

    added 2025 day 10

---
+++ test/advent/year2025/day10_test.exs
@@ -0,0 +1,19 @@
+defmodule Advent.Year2025.Day10Test do
+  use ExUnit.Case
+
+  import Elixir.Advent.Year2025.Day10
+
+  @input ""
+
+  test "part1" do
+    result = part1(@input)
+
+    assert result
+  end
+
+  test "part2" do
+    result = part2(@input)
+
+    assert result
+  end
+end
